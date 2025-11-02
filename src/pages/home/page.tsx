61ef23df33e9b98124c997a9664b77634cd7a8a2
Pedro Baptista <pedrolk2012@gmail.com>
2025-11-02 13:40:26 -0300

    first commit

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles/style.css";
+
+export function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold mb-4">
+          🚀 Vite + React + Tailwind + Electron
+        </h1>
+        <button className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded-xl transition-all">
+          Click Me
+        </button>
+        <button onClick={() => navigate("/second")}>
+          Go to Second Page ➡️
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/second/page.tsx
@@ -0,0 +1,8 @@
+export function SecondPage() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Welcome to the Second Page 🎉</h1>
+      <p>This is another React route inside your Electron app.</p>
+    </div>
+  );
+}
